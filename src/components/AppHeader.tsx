3fb656f92b1e2ddaf168e1c337331c24dfa5410a
maylton <maylton.fernandes@gmail.com>
2026-08-18 20:48:10 -0300

    replace header UI symbol with Bootstrap icon

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { Brand } from "./Brand";
 import { AuthStatus } from "./AuthStatus";
+import { AppIcon } from "./AppIcon";
 import { SettingsPanel } from "./settings/SettingsPanel";
 
 export function AppHeader() {
@@ -12,7 +13,7 @@ export function AppHeader() {
         <Link href="/join">Join</Link>
         <SettingsPanel />
         <AuthStatus />
-        <Link className="button button-primary button-small" href="/create">+ Create activity</Link>
+        <Link className="button button-primary button-small" href="/create"><AppIcon name="plus-lg" /> Create activity</Link>
       </nav>
     </header>
   );
